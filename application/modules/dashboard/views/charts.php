3faba0271a756540a60aaa45c868c3f298a0e327
Agaba Andre <agabaandre@gmail.com>
2025-03-24 10:24:22 +0300

    fixed auth

--- application/modules/dashboard/views/charts.php
+++ application/modules/dashboard/views/charts.php
@@ -3,7 +3,7 @@
     <div class="col-md-3 p-2">
   <div class="card">
     <div class="card-body">
-      <div id="disease-probability-gauge" style="height: 270px;"></div>
+      <div id="disease-probability-gauge" style="height: 255px;"></div>
 
       <!-- Legend / Key -->
       <div class="mt-3 text-center">
